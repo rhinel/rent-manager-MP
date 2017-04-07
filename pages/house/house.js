08bd4c739a0a52c437b4686f7675273e88cd577b
rhinel <rhinelkc@hotmail.com>
2017-04-08 00:46:58 +0800

    完成月度详情页和房屋列表页

--- pages/house/house.js
+++ pages/house/house.js
@@ -1,10 +1,28 @@
 // house.js
 let ajax = require('../../assets/utils/request.js')
+let formatDate = require('../../assets/utils/util.js').formatDate
 Page({
     data: {
-
+        filter: '',
+        filterShowed: false,
+        houseDate: [],
+        houseDateFiltered: [],
+        typesVal: ['', '已交', '给单', '房东'],
+        payTypeVal: ['微信', '支付宝', '银行转账', '现金']
     },
-    onLoad: function (options) {
+    onLoad(options) {
+        wx.showToast({
+            title: '加载中',
+            icon: 'loading',
+            duration: 2000000
+        })
+        Promise.all([
+            new Promise((resolve) => {
+                this.bindGetHouse(resolve)
+            })
+        ]).then((data) => {
+            wx.hideToast()
+        })
         // 生命周期函数--监听页面加载
         ajax('/inner/auth/check', {}, (res) => { }, (res) => {
             wx.reLaunch({
@@ -12,28 +30,108 @@ Page({
             })
         })
     },
-    onReady: function () {
+    onReady() {
         // 生命周期函数--监听页面初次渲染完成
 
     },
-    onShow: function () {
+    onShow() {
         // 生命周期函数--监听页面显示
 
     },
-    onHide: function () {
+    onHide() {
         // 生命周期函数--监听页面隐藏
 
     },
-    onUnload: function () {
+    onUnload() {
         // 生命周期函数--监听页面卸载
 
     },
-    onPullDownRefresh: function () {
+    onPullDownRefresh() {
         // 页面相关事件处理函数--监听用户下拉动作
-        wx.stopPullDownRefresh()
+        Promise.all([
+            new Promise((resolve) => {
+                this.bindGetHouse(resolve)
+            })
+        ]).then((data) => {
+            wx.stopPullDownRefresh()
+            wx.showToast({
+                title: '获取成功',
+                icon: 'success',
+                duration: 1000
+            })
+        })
+        return false
     },
-    onReachBottom: function () {
+    onReachBottom() {
         // 页面上拉触底事件的处理函数
-
+        return false
+    },
+    bindGetHouse(resolve) {
+        let that = this
+        ajax('/inner/lease/mainList', {}, (res) => {
+            res.data.data.forEach((i) => {
+                i.leaseId.status == 1 && (i.leaseId.leaserange[1] = that.bindGetFormatDate(i.leaseId.leaserange[1]))
+            })
+            that.setData({
+                houseDate: res.data.data
+            })
+            that.bindGetFilterDate()
+            resolve && resolve()
+        }, (res) => {
+            wx.showToast({
+                title: String(res.data.msg),
+                image: '../../assets/error.png',
+                icon: 'loading',
+                duration: 2000
+            })
+        })
+    },
+    bindGetFilterDate() {
+        let that = this
+        let filter = []
+        if (!that.data.filter) {
+            filter = that.data.houseDate
+        } else {
+            filter = that.data.houseDate.filter((i) => {
+                return String(i.fanghao).match(that.data.filter)
+            })
+        }
+        that.setData({
+            houseDateFiltered: filter
+        })
+    },
+    bindGetFilter(e) {
+        this.setData({
+            filter: e.detail.value
+        })
+        this.bindGetFilterDate()
+    },
+    bindGetShowFilter() {
+        this.setData({
+            filterShowed: true
+        })
+    },
+    bindGetHideFilter() {
+        this.setData({
+            filter: '',
+            filterShowed: false
+        })
+        this.bindGetFilterDate()
+    },
+    bindGetClearFilter() {
+        this.setData({
+            filter: '',
+            filterShowed: true
+        })
+        this.bindGetFilterDate()
+    },
+    bindGetFormatDate(v) {
+        return v && formatDate(new Date(v))
+    },
+    bindGoToDet(e) {
+        let that = this
+        wx.navigateTo({
+            url: '/pages/house-det/house-det?id=' + e.currentTarget.dataset.id
+        })
     }
 })
--- pages/month-rent/month-rent.js
+++ pages/month-rent/month-rent.js
@@ -6,7 +6,7 @@ Page({
         loaded: false,
         haoId: '',
         monthId: '',
-        det: {},
+        detList: {},
         typesVal: ['', '已交', '给单', '房东'],
         payTypeVal: ['微信', '支付宝', '银行转账', '现金']
     },
@@ -78,28 +78,31 @@ Page({
             haoId: that.data.haoId,
             monthId: that.data.monthId
         }, (res) => {
-            let _data = res.data.data[0]
-            if (_data) {
-                // 租住的时间显示
-                _data.lease.addTime = that.bindGetFormatDate(_data.lease.addTime)
-                _data.lease.leaserange && (_data.lease.leaserange[0] = that.bindGetFormatDate(_data.lease.leaserange[0]))
-                _data.lease.leaserange && (_data.lease.leaserange[1] = that.bindGetFormatDate(_data.lease.leaserange[1]))
-                // 水表的时间显示
-                _data.calWater.addTime = that.bindGetFormatDate(_data.calWater.addTime)
-                _data.calWater.tnew.addTime = that.bindGetFormatDate(_data.calWater.tnew.addTime)
-                _data.calWater.old.addTime = that.bindGetFormatDate(_data.calWater.old.addTime)
-                // 电表的时间显示
-                _data.calElectric.addTime = that.bindGetFormatDate(_data.calElectric.addTime)
-                _data.calElectric.tnew.addTime = that.bindGetFormatDate(_data.calElectric.tnew.addTime)
-                _data.calElectric.old.addTime = that.bindGetFormatDate(_data.calElectric.old.addTime)
-                // 计租时间
-                _data.addTime = that.bindGetFormatDate(_data.addTime)
-                for (var i in _data.type.typeTime) {
-                    _data.type.typeTime[i] = that.bindGetFormatDate(_data.type.typeTime[i])
+            let r_data = res.data.data
+            r_data.forEach((_data) => {
+                if (_data) {
+                    // 租住的时间显示
+                    _data.lease.addTime = that.bindGetFormatDate(_data.lease.addTime)
+                    _data.lease.leaserange && (_data.lease.leaserange[0] = that.bindGetFormatDate(_data.lease.leaserange[0]))
+                    _data.lease.leaserange && (_data.lease.leaserange[1] = that.bindGetFormatDate(_data.lease.leaserange[1]))
+                    // 水表的时间显示
+                    _data.calWater.addTime = that.bindGetFormatDate(_data.calWater.addTime)
+                    _data.calWater.tnew.addTime = that.bindGetFormatDate(_data.calWater.tnew.addTime)
+                    _data.calWater.old.addTime = that.bindGetFormatDate(_data.calWater.old.addTime)
+                    // 电表的时间显示
+                    _data.calElectric.addTime = that.bindGetFormatDate(_data.calElectric.addTime)
+                    _data.calElectric.tnew.addTime = that.bindGetFormatDate(_data.calElectric.tnew.addTime)
+                    _data.calElectric.old.addTime = that.bindGetFormatDate(_data.calElectric.old.addTime)
+                    // 计租时间
+                    _data.addTime = that.bindGetFormatDate(_data.addTime)
+                    for (var i in _data.type.typeTime) {
+                        _data.type.typeTime[i] = that.bindGetFormatDate(_data.type.typeTime[i])
+                    }
+                    _data.show = false
                 }
-            }
+            })
             that.setData({
-                det: _data
+                detList: r_data
             })
             wx.setNavigationBarTitle({
                 title: res.data.data[0] ? res.data.data[0].fanghao : '暂无数据'
@@ -116,5 +119,11 @@ Page({
     },
     bindGetFormatDate(v) {
         return v && formatDate(new Date(v))
+    },
+    bindToggle(e) {
+        let that = this
+        let param = {}
+        param['detList[' + e.currentTarget.dataset.index + '].show'] = !that.data.detList[e.currentTarget.dataset.index].show
+        that.setData(param)
     }
 })
